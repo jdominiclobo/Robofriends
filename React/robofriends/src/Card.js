7cae68b101d4583ee1486c4fd5ac32d4cd96002c
joel dominic lobo <joe888dom@gmail.com>
2021-05-16 15:40:47 +0530

    robofriends initial commit

---
+++ React/robofriends/src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="bg-light-green">
+      <img alt="robot" src="https://robohash.org/test?200x200" />
+      <div>
+        <h2>Joel</h2>
+        <p>joel@email.com</p>
+      </div>
+    </div>
+  );
+};
+export default Card;
